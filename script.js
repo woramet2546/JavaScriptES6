baccfb709c01616f5e6d589bb2d46ebd50c80158
Woramet Srisawang <pozachannel@gmail.com>
2024-08-02 18:17:45 +0700

    Update 1

---
+++ script.js
@@ -0,0 +1,11 @@
+// ค่าทุกค่าจะมาจากการกำหนด ID หน้า index.html
+//ด้านหน้าให้สร้างตัวแปรเก็บค่า = ด้านหลังของ getElementByid จะเป็นชื่อ ID
+
+const currency_one=document.getElementById('currency-one');
+const currency_two=document.getElementById('currency-two');
+
+const amount_one=document.getElementById('amount-one')
+const amount_two=document.getElementById('amount-two')
+
+const rateText = document.getElementById('rate');
+const swap = document.getElementById('btn');
