6902ee79fd4d8e4749b4f55b655e10a50ac2f418
Woramet Srisawang <pozachannel@gmail.com>
2024-08-02 19:39:37 +0700

    Update chang

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ const amount_two=document.getElementById('amount-two')
 
 const rateText = document.getElementById('rate');
 const swap = document.getElementById('btn');
+
+currency_one.addEventListener('change',calculateMoney);
+currency_two.addEventListener('change',calculateMoney);
+
+amount_one.addEventListener('input',calculateMoney);
+amount_two.addEventListener('input',calculateMoney);
+
+function calculateMoney(){
+    const one = currency_one.value;
+    const two = currency_two.value;
+    fetch(`https://api.exchangerate-api.com/v4/latest/${one}`)
+    .then(res=>res.json()).then(data=>{
+        const rate=data.rates[two]
+        rateText.innerText=`1 ${one} = ${rate}${two}`;
+        amount_two.value=(amount_one.value*rate);
+    })
+
+    // ขั้นตอนการส้รางสลับสกุลเงิน
+}
+swap.addEventListener('click',()=>{
+    const temp = currency_one.value; //ต้นทาง
+    currency_one.value = currency_two.value;
+    currency_two.value = temp;
+    calculateMoney();
+})
+
+calculateMoney();
